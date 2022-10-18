0fa54f72ec1582fce538981d401627ade3c12ce8
Abilash1503 <115686328+Abilash1503@users.noreply.github.com>
2022-10-18 15:36:18 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,60 @@
+const resume =[
+    {
+       "Name":"Abilash"
+    }
+]
+    const careerobjective=
+[
+{
+     
+       "careerobjective": "To obtain a position that challenges me and provides me the opportunity to reach  my full potential professionally and personally utilizing my abilities and years of experience in your organization that is progressive intellectually and technically and one in which practices collaborative leadership, integrity and honesty",
+    }
+    ] 
+    const qualification =[
+    {
+
+       "degree":"B.Com INTERNATIONAL BUSINESS",
+       "HSC": "SHRI K KRISHNASWAMY NAIDU MEMORIAL HIGHER SECONDARY SCHOOL",
+       "SSLC": "GANDHIMAANAGAR GOVERNMENT HIGH SCHOOL",
+       }
+    ]
+    const AREAOFINTEREST =[
+        {
+            "AREA OF INTEREST":"Basically I’m a  Non - ΙΤ Background person ,Ineed to switch toIT sector so I completed full stack developer course in guvi – IIT Madras Analyst Associate Analyst",
+
+           
+        }
+    ]
+       const EXTRACURRICULARACTIVITIES =[
+        {
+            "EXTRA CURRICULAR ACTIVITIES":"Got 1st prize for District Level WrestlingCompetition.",
+
+        }
+       ]
+      const otherdetiles =[
+        {
+            "farther name":"dhanabal",
+            "mother name":"padmini",
+            "mobile number":"9943767563"
+        }
+      ]
+       
+       
+
+    
+       
+        
+
+
+
+    
+
+console.log(resume);
+console.log(careerobjective);
+console.log(qualification);
+console.log(AREAOFINTEREST);
+console.log(EXTRACURRICULARACTIVITIES);
+console.log(otherdetiles);
+
+
+
